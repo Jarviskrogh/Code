68ea9f20b006e26acb79bbe19a416a25f0c15e11
Bethany Jarvis <brjarvis@wisc.edu>
2025-07-03 11:31:16 -0500

    Created graphing class and renamed TestAPI to convertNetCDF

--- glacier_work.py
+++ glacier_work.py
@@ -4,42 +4,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
 import itslive
+from convertNetCDF import convertToNetCDF
 
-#glacier_file_ = 'C:\\Users\\betha\\Desktop\LaserGlacier\ITS_LIVE_velocity_120m_RGI07A_2012_v02.nc'
-glacier_file = "/home/brjarvis/Documents/LASERGlacier/ITS_LIVE_velocity_120m_RGI07A_2012_v02.nc"
-xrds = xr.open_dataset(glacier_file)
+class graphing(convertToNetCDF):
+    def __init__(self, points, name):
+        super().__init__(points, name)
+        try:
+            self.xrds = xr.open_dataset(self.nameForFile, decode_timedelta=True)
+        except:
+            convertTime = convertToNetCDF(self.origPoints, self.nameForFile)
+            convertTime.convertFile()
+            self.xrds = xr.open_dataset(self.nameForFile, decode_timedelta=True)
+    
+    def plotV(self):
+        self.xrds['v'].plot()
+        plt.show()
 
-#print(xrds)
-#for attr, value in xrds.attrs.items():
-#    print(f"{attr}: {value} \n")
-#print(xrds.data_vars)
-#print(xrds.data_vars['v_error'])
-#print(xrds.data_vars['v_error'].attrs)
-#print(xrds.data_vars['v_error'].values)
-#print(xrds.data_vars['landice'])
-
-#xrds['mapping'].plot()
-#xrds['count'].plot()
-#xrds['landice'].plot()
-#xrds['floatingice'].plot()
-#xrds['v'].plot()
-#xrds['vx'].plot()
-#xrds['vy'].plot()
-#xrds['v_error'].plot()
-#xrds['vx_error'].plot()
-#xrds['vy_error'].plot()
-#plt.show()
-
-velocity = xrds['v'].to_dataframe()
-df = xrds.to_dataframe()
-#df.to_csv('C:\\Users\\betha\\Desktop\\LaserGlacier\\allData')
-#print(df)
-
-xSlice = xrds.sel(x = 900532.5)
-xSliceFrame = xSlice.to_dataframe()
-print(xSliceFrame)
-#xSliceFrame.to_csv('C:\\Users\\betha\\Desktop\\LaserGlacier\\xSlice')
-
+point = [(19.4, 78.8)]
+name = 'glacierVel.nc'
+graph = graphing(point, name)
+graph.plotV()
+            
 
 
 
