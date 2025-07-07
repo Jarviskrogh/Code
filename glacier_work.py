780d9661ceb9d20564f8d71b65f01e7549d5e703
Bethany Jarvis <brjarvis@wisc.edu>
2025-07-07 17:33:57 -0500

    added more plot functions

--- glacier_work.py
+++ glacier_work.py
@@ -17,15 +17,55 @@ class graphing(convertToNetCDF):
             self.xrds = xr.open_dataset(self.nameForFile, decode_timedelta=True)
     
     def plotV(self):
-        self.xrds['v'].plot()
+        #self.xrds['v'].plot.scatter()
+        #xr.plot.line(self.xrds['v'].dropna('mid_date'))
+        date = self.xrds.sel(mid_date='2019')
+        date['v'].plot.scatter()
+        plt.show()
+    
+    def plotVX(self):
+        #self.xrds['vx'].plot.scatter()
+        #xr.plot.line(self.xrds['vx'].dropna('mid_date'))
+        date = self.xrds.sel(mid_date='2019')
+        date['vx'].plot.scatter()
         plt.show()
 
+    def plotVY(self):
+        #self.xrds['vy'].plot.scatter()
+        #xr.plot.line(self.xrds['vy'].dropna('mid_date'))
+        date = self.xrds.sel(mid_date='2019')
+        date['vy'].plot.scatter()
+        plt.show()
+
+    def plotV_Error(self):
+        #self.xrds['v_error'].plot.scatter()
+        #xr.plot.line(self.xrds['v_error'].dropna('mid_date'))
+        date = self.xrds.sel(mid_date='2019')
+        date['v_error'].plot.scatter()
+        plt.show()
+
+    def plotVX_Error(self):
+        #self.xrds['vx_error'].plot.scatter()
+        #xr.plot.line(self.xrds['vx_error'].dropna('mid_date'))
+        date = self.xrds.sel(mid_date='2019')
+        date['vx_error'].plot.scatter()
+        plt.show()
+
+    def plotVY_Error(self):
+        #self.xrds['vy_error'].plot.scatter()
+        #xr.plot.line(self.xrds['vy_error'].dropna('mid_date'))
+        date = self.xrds.sel(mid_date='2019')
+        date['vy_error'].plot.scatter()
+        plt.show()
+
+
+
+
 point = [(19.4, 78.8)]
-name = 'glacierVel.nc'
+name = 'glacierDATA.nc'
 graph = graphing(point, name)
-# graph.xrds['v'].plot()
-# plt.show()
-#print(graph.xrds)
+graph.plotV()
+#print(graph.xrds['v'].attrs['description'])
 #print(graph.xrds.data_vars) #Data vars: date_dt, v, vx_error, vy, vy_error, v_error, vx, mission_img1, satellite_img1,
 #   lon, lat, x_proj, y_proj
 df = graph.xrds.to_dataframe()
